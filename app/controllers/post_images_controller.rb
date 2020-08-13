0647f7a540a378d36efe2efb2f5b39867e0d09a6
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-14 00:59:58 +0900

    [Add]画像投稿機能実装

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,9 +1,14 @@
 class PostImagesController < ApplicationController
 
   def new
+  	@post_image = PostImage.new
   end
 
   def create
+  	@post_image = PostImage.new(post_image_params)
+  	@post_image.user_id = current_user.user_id
+  	@post_image.save
+  	redirect_to post_images_path
   end
 
   def index
@@ -12,4 +17,8 @@ class PostImagesController < ApplicationController
   def show
   end
   
+  private
+  def post_image_params
+  	params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
 end
